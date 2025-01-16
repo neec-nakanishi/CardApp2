b63277dbe9f9731c8699bd349b316536fdfe6301
neec-nakanishi <nakanishi@g.neec.ac.jp>
2025-01-16 09:27:35 +0900

    修正

--- Player.java
+++ Player.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Player {
     Card turn(Card[] cards) {
+        // Enterキーを押したらサイコロを振るようにする
+        Scanner scan = new Scanner(System.in);
+        scan.nextLine();
         int rnd = (int)(Math.random() * cards.length);
         return cards[rnd];
     }
